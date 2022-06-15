7c275def6f0540b8c4f80440f42cb9cc2d5707e5
faruqmaulana <faruqchodot21@gmail.com>
2022-06-15 16:43:14 +0700

    fix partials order

--- resources/views/order/partials/orderNavbar.blade.php
+++ resources/views/order/partials/orderNavbar.blade.php
@@ -1,26 +1,29 @@
 <nav class="navbar navbar-expand-lg navbar-success py-2 sticky-top bg-white shadow">
   <div class="container">
     <a class="d-flex navbar-brand justify-content-start" href="/">
-      <img src="assets/logo.png" alt="" width="184" height="57">
+      <img src="../assets/logo.png" alt="" width="184" height="57">
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="d-flex collapse navbar-collapse justify-content-center" id="navbarNav">
-        <ul class="navbar-nav gap-3">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav" style="padding-right: 184px;">
+        <ul class="navbar-nav gap-3 d-flex align-items-center">
           <li class="nav-item">
               <a class="nav-link font-md font-regular" href="/order">ORDER</a>
-          </li> 
+            </li> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+          </svg>
           <li class="nav-item">
               <a class="nav-link font-md font-regular" href="/order/review">REVIEW</a>
           </li>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">
+            <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+          </svg>
           <li class="nav-item">
               <a class="nav-link font-md font-regular active" href="/order/eticket">E-TICKET</a>
           </li>
         </ul>
-    </div>
-    <div class="collapse navbar-collapse justify-content-end flex-grow-0 gap-4" id="navbarNav">
-        
     </div>
   </div>
 </nav>
