512795cc9a7e41fc8a4a0f36757318f39889f731
Agustinus Frinsen Farrelino Yoses <frinsenfarrelino@gmail.com>
2022-06-15 16:10:10 +0700

    membuat partial pada order

---
+++ resources/views/order/partials/orderFooter.blade.php
@@ -0,0 +1,10 @@
+<footer>
+  <div class="d-flex justify-content-center">
+    <p>2022 PT.Tiketku. All Right Reserved</p><br><br>
+    <img src="assets/logo.png" alt="" width="184" height="57">
+    
+  </div>
+  
+  
+</footer>
+
