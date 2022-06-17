f8905eb690ea9b564939a781b56efb50112af6e2
aditya <adityaputraprasetya2@gmail.com>
2022-06-17 23:19:14 +0700

    fix delete button di view my-ticket

--- resources/views/profile/my-ticket.blade.php
+++ resources/views/profile/my-ticket.blade.php
@@ -72,7 +72,9 @@
             </div>
           </div>
           <div class="col-1 my-auto" style="width: 1rem">
-            <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            <a href="#" class="btn btn-default"  role="button" aria-disabled="true">              
+              <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            </a>
           </div>
         </div>
 
@@ -122,7 +124,61 @@
             </div>
           </div>
           <div class="col-1 my-auto" style="width: 1rem">
-            <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            <a href="#" class="btn btn-default"  role="button" aria-disabled="true">              
+              <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            </a>
+          </div>
+        </div>
+
+        <div class="row mb-4">
+          <div class="col">
+            <div class="card" style="width: 100%;">
+              <div class="card-body" style="background-color: #D9D9D9">
+               <div class="row ">
+    
+                <div class="col d-flex flex-column" >              
+                  <p class="my-auto mb-4">
+                    <span class="fw-bold">Airlines</span> <br>
+                    <small> Qatar Airways</small>
+                  </p>
+                  <p class="my-auto">
+                    <span class="fw-bold">Seat Class</span> <br>
+                    <small> Economy</small>
+                  </p>
+                </div>
+    
+                <div class="col d-flex flex-column">
+                  <p class="my-auto mb-4">
+                    <span class="fw-bold">From</span> <br>
+                    <small>New York(NYC)</small>
+                  </p>
+                  <p class="my-auto">
+                    <span class="fw-bold">To</span> <br>
+                    <small> London(LON)</small>
+                  </p>
+                </div>
+    
+                <div class="col d-flex flex-column">
+                  <p class="my-auto mb-4">
+                    <span class="fw-bold">Departure Date</span> <br>
+                    <small> Qatar Airways</small>
+                  </p>
+                  <p class="my-auto">
+                    <span class="fw-bold">No. of Passengers</span> <br>
+                    <small>2</small>
+                  </p>
+                </div>    
+                <div class="col d-flex flex-column " >
+                  <a href="#" class="d-flex align-self-center rounded-pill btn-md btn btn-outline-danger px-4 my-auto" tabindex="-1" role="button" aria-disabled="true" style="btn:hover{color: var(--white-color)}; width:140px">Show Detail</a>
+                </div>              
+               </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-1 my-auto" style="width: 1rem">            
+            <a href="#" class="btn btn-default"  role="button" aria-disabled="true">              
+              <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            </a>
           </div>
         </div>
 
@@ -172,57 +228,9 @@
             </div>
           </div>
           <div class="col-1 my-auto" style="width: 1rem">
-            <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
-          </div>
-        </div>
-
-        <div class="row mb-4">
-          <div class="col">
-            <div class="card" style="width: 100%;">
-              <div class="card-body" style="background-color: #D9D9D9">
-               <div class="row ">
-    
-                <div class="col d-flex flex-column" >              
-                  <p class="my-auto mb-4">
-                    <span class="fw-bold">Airlines</span> <br>
-                    <small> Qatar Airways</small>
-                  </p>
-                  <p class="my-auto">
-                    <span class="fw-bold">Seat Class</span> <br>
-                    <small> Economy</small>
-                  </p>
-                </div>
-    
-                <div class="col d-flex flex-column">
-                  <p class="my-auto mb-4">
-                    <span class="fw-bold">From</span> <br>
-                    <small>New York(NYC)</small>
-                  </p>
-                  <p class="my-auto">
-                    <span class="fw-bold">To</span> <br>
-                    <small> London(LON)</small>
-                  </p>
-                </div>
-    
-                <div class="col d-flex flex-column">
-                  <p class="my-auto mb-4">
-                    <span class="fw-bold">Departure Date</span> <br>
-                    <small> Qatar Airways</small>
-                  </p>
-                  <p class="my-auto">
-                    <span class="fw-bold">No. of Passengers</span> <br>
-                    <small>2</small>
-                  </p>
-                </div>    
-                <div class="col d-flex flex-column " >
-                  <a href="#" class="d-flex align-self-center rounded-pill btn-md btn btn-outline-danger px-4 my-auto" tabindex="-1" role="button" aria-disabled="true" style="btn:hover{color: var(--white-color)}; width:140px">Show Detail</a>
-                </div>              
-               </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-1 my-auto" style="width: 1rem">
-            <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            <a href="#" class="btn btn-default"  role="button" aria-disabled="true">              
+              <img src="assets/my-ticket/trash.png" style="width: 30px"  alt="" class="align-self-center">          
+            </a>
           </div>
         </div>
 
