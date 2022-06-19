d903a2a6e6070893786a7d1e2f6f5998bc3b713e
Agustinus Frinsen Farrelino Yoses <frinsenfarrelino@gmail.com>
2022-06-19 21:31:42 +0700

    completing page eticket

--- resources/views/order/eticket.blade.php
+++ resources/views/order/eticket.blade.php
@@ -40,8 +40,66 @@
     </div>
   </div>
   <hr class="border-3">
-  
+  <div class="d-flex justify-content-between">
+    <div class="d-flex">
+      <img src="../assets/eticket/Identity.png" alt="" width="100px">
+      <p class="ms-4 me-4">Show your ticket and valid identity card in the airport</p>
+    </div>
+    <div>
+      
+    </div>
+    <div class="d-flex">
+      <img src="../assets/eticket/Check-in.png" alt="" width="100px">
+      <p class="ms-4 me-4">You must check-in atleast 90 minutes before departure</p>
+    </div>
+    <div class="d-flex">
+      <img src="../assets/eticket/Information.png" alt="" width="100px">
+      <p class="ms-4">Time format is depending on the airport location</p>
+    </div>
+  </div>
   <hr>
+  <p class="title-section-primary pt-2 mb-5">Passenger Detail</p>
+  <div class="border border-secondary rounded-4">
+    <div class="d-flex justify-content-center mt-4 mb-4 ms-4 me-4">
+      <table class="table table-borderless">
+        <thead>
+          <tr>
+            <th scope="col">No</th>
+            <th scope="col">Passengers</th>
+            <th scope="col">Flight Number</th>
+            <th scope="col">Baggage</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Agustinus Frinsen Farrelino Yoses</td>
+            <td>FN-003</td>
+            <td>20kg</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Joshua Adrian Maulana Ronaldy</td>
+            <td>FN-003</td>
+            <td>20kg</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <div class="d-flex justify-content-center mt-5">
+    <button class="btn btn-red font-md font-white font-bold btn-search d-flex align-items-center justify-content-center">
+      <span style="padding-left: 10px">
+          GO TO MY TICKET
+      </span>
+    </button>
+  </div>
+
+  <div class="d-flex justify-content-center mt-3 mb-5">
+    <a href="" class="mt-2 text-decoration-none text-black fw-semibold">BACK TO HOME</a>
+  </div>
+
 </div>
 
 @endsection
